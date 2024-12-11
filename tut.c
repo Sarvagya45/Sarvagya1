7c47d2f2b09356bd3bacbf94bd44f82d0ed8ca72
gsarvagya45 <gsarvagya45@gmail.com>
2024-12-12 00:24:52 +0530

    added commands regarding git

---
+++ tut.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main(){
+    printf("hello world");
+    return 0;
+}
+
+
+//to configure account command
+/*
+git config global user.name "name as per choice"
+git config global user.email "email jisse github account hai"
+git config --list
+*/
+
+
+//clone and status command
+/*
+git clone "link of repo"
+git status
+ */
+
+//pneumonics
+/*
+untracked - new files that git doesn't yet track
+modified - changed file
+staged - file is ready to be commited "means ki yeh humne add kari hai na ki commit kari commit baad me karenge"
+unmodified - unchanged file
+ */
+
+//to add and commit command
+/*
+git add 'filename' OR 
+if we have to add many changes at one time 
+git add .
+git 
+*/
